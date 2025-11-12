fb7f9dd55f30a301cec9453265c60b28a28dbf56
narayanbehera14 <beheranarayancs222330@gmail.com>
2025-11-12 15:36:11 +0530

    updated

---
+++ main.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+chrome_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+options = Options()
+options.binary_location = chrome_path
+
+print("Starting Chrome browser...")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://www.google.com")
+print("Opened:", driver.title)
+
+time.sleep(5)
+driver.quit()
